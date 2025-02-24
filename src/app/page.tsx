5db44ef4c7e4d7b935dfa114cbb17bba020977c5
Shraddha <ms.shraddha14@gmail.com>
2025-02-23 20:56:38 -0600

    changed about description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+import styles from './page.module.css'
+import { neue } from "@/utils/fontConfig";
+
 export default function Home() {
   return (
-    <main >
-    </main>
+    // <div className={styles.mainContent}>
+    <>
+      <div className={`${neue.className} ${styles.mainHeading}`}>
+        <div className={styles.headingContainer}>
+          <span className={styles.name}>
+          <span className={styles.nameFirstPara}>Hey! I am </span>
+          <span className={styles.nameSecondPara}>Shraddha <span className={styles.dot}>.</span></span> 
+          </span>
+          <span className={styles.description}>
+          I thrive at the intersection of technology and strategy, bridging the gap between development and business to create impactful solutions.
+          </span>
+        </div>
+        <div className={styles.imageContainer}/>
+      </div>
+      
+        {/* </div> */}
+
+          {/* <img src="/assests/Group.png" alt="" className={styles.groupedImage}/> */}
+    </>
   );
 }
